8a8eb896c3162a994ffcd5412b1b9ca2d1351360
Amaan Siddiqui <amaansiddiqui014@gmail.com>
2025-11-08 00:07:58 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Starting the Chicken Disease Classification Application")
 
 
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
